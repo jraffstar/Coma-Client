5bc0bb7bae0cdf443d300a18ab61cec044ee0c9e
Jen <jraffstar@gmail.com>
2021-10-08 21:15:33 +0100

    Rename da stuff

--- src/main/java/me/hollow/trollgod/api/interfaces/Minecraftable.java
+++
@@ -1,8 +0,0 @@
-package me.hollow.trollgod.api.interfaces;
-
-import net.minecraft.client.Minecraft;
-
-public interface Minecraftable {
-    Minecraft mc = Minecraft.getMinecraft();
-}
-
--- src/main/java/me/hollow/trollgod/api/mixin/mixins/client/MixinAbstractClientPlayer.java
+++
@@ -1,9 +0,0 @@
-package me.hollow.trollgod.api.mixin.mixins.client;
-
-import net.minecraft.client.entity.AbstractClientPlayer;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value={AbstractClientPlayer.class})
-public class MixinAbstractClientPlayer {
-}
-
